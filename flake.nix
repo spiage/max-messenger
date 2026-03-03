df9907c452e4cf7c6a0bfd1c07d3e9fe590f568e
spiage <spiage@mail.ru>
2026-03-03 06:45:04 +0300

    new native non-FHS version

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
-  description = "MAX Messenger for NixOS (FHS Env)";
+  description = "MAX Messenger Native for NixOS (No Service)";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -15,114 +15,163 @@
         };
 
         version = "26.5.1";
-        debFile = "MAX-26.5.1.48203.deb";
+        debFile = "MAX-${version}.48203.deb";
         srcUrl = "https://download.max.ru/linux/deb/pool/main/m/max/${debFile}";
         srcHash = "sha256-Wralrk1JzfL96jfGQvdgqHeIv46xSDlL/rT4E8v0Sb0=";
 
-        max-unpacked = pkgs.stdenv.mkDerivation {
-          pname = "max-contents";
-          inherit version;
-          src = pkgs.fetchurl { url = srcUrl; hash = srcHash; };
-          
-          nativeBuildInputs = [ pkgs.dpkg ];
-          
-          installPhase = ''
-            mkdir -p $out
-            dpkg -x $src .
-            if [ -d "usr" ]; then cp -r usr/* $out/; fi
-            if [ -d "opt" ]; then mkdir -p $out/opt && cp -r opt/* $out/opt/; fi
-            
-            # Удаляем битой симлинк
-            find $out -type l -name "libtracernative.so" -exec rm -f {} \;
-
-            # --- ИСПРАВЛЕНИЕ КОНФЛИКТА БИБЛИОТЕК ---
-            # Создаем wrapper для max-service, чтобы он использовал свои библиотеки в приоритете.
-            # Это решает ошибку "undefined symbol" при запуске сервиса.
-            
-            SVC_DIR="$out/share/max/bin/max-service"
-            SVC_BIN="$SVC_DIR/bin/max-service"
-
-            # Переименовываем оригинальный бинарник
-            mv "$SVC_BIN" "$SVC_BIN.bin"
-
-            # Создаем скрипт-обертку
-            cat <<EOF > "$SVC_BIN"
-            #!/bin/sh
-            # Меняем порядок поиска: сначала библиотеки сервиса, потом общие, потом системные
-            export LD_LIBRARY_PATH="$SVC_DIR/lib64:/usr/share/max/lib64:/usr/lib:/usr/lib64"
-            exec "$SVC_BIN.bin" "\$@"
-            EOF
-            chmod +x "$SVC_BIN"
-          '';
-        };
-
-        max-fhs = pkgs.buildFHSEnv {
-          name = "max-fhs";
-
-          targetPkgs = pkgs: with pkgs; [
-            # Основные системные библиотеки
-            zlib zstd glib nss nspr expat openssl alsa-lib cups dbus fontconfig freetype
-            
-            # Зависимости для GLib/GIO
-            util-linux libselinux libsepol
-            
-            # Графика и мультимедиа
-            gdk-pixbuf libgbm libGL libglvnd mesa libdrm
-            libpulseaudio pipewire
-            libxkbcommon wayland
-            krb5
-            
-            # X11 Libraries (Базовые)
-            libX11 libXcomposite libXcursor libXdamage libXext
-            libXfixes libXi libXrandr libXrender libXScrnSaver
-            libXtst libxcb
-            libice libsm
-            libxshmfence libxkbfile
-            
-            # X11 Libraries (Для max-service)
-            libxt
-            libxv
-            libxaw
-            libxmu
-            libxpm
-            libxinerama
-            libxxf86vm
-            libfontenc
-            libxres
-            libxau
-            libxdmcp
-            
-            # xcb утилиты
-            libxcb-cursor     
-            libxcb-keysyms
-            libxcb-wm
-            libxcb-util
-            libxcb-image
-            libxcb-render-util
-
-            libnotify libva libvdpau pango
-            
-            # Трей
-            libappindicator-gtk3
-
-            max-unpacked
-          ];
-
-          runScript = pkgs.writeShellScript "start-max" ''
-            export QT_QPA_PLATFORM="wayland;xcb"
-            
-            # Стандартный путь для клиента: системные -> библиотеки приложения -> библиотеки сервиса
-            export LD_LIBRARY_PATH=/usr/lib:/usr/lib64:/usr/share/max/lib64:/usr/share/max/bin/max-service/lib64
-            
-            export QT_PLUGIN_PATH="/usr/share/max/plugins:/usr/share/max/bin/max-service/plugins"
-
-            exec /usr/share/max/bin/max "$@"
-          '';
+        desktopItem = pkgs.makeDesktopItem {
+          name = "max";
+          exec = "max %U";
+          icon = "max";
+          desktopName = "MAX";
+          categories = [ "Network" "InstantMessaging" ];
+          mimeTypes = [ "x-scheme-handler/max" ];
+          terminal = false;
         };
 
       in
       {
-        packages.default = max-fhs;
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "max-native";
+          inherit version;
+
+          src = pkgs.fetchurl {
+            url = srcUrl;
+            hash = srcHash;
+          };
+
+          dontWrapQtApps = true;
+
+          nativeBuildInputs = with pkgs; [
+            autoPatchelfHook
+            dpkg
+            desktop-file-utils
+            makeWrapper
+            copyDesktopItems
+          ];
+
+          buildInputs = with pkgs; [
+            # GL / OpenGL
+            libglvnd
+            
+            # GLib
+            glib
+            gdk-pixbuf
+            
+            # X11 / XCB
+            libX11 libXcomposite libXdamage libXext libXfixes libXrender
+            libXrandr libXtst libXi libxcb libxkbfile libxshmfence
+            libxcb-wm 
+            
+            # Input
+            libxkbcommon
+            
+            # Font
+            fontconfig freetype expat
+            
+            # Audio
+            pulseaudio alsa-lib
+            
+            # Notification / DBus
+            libnotify dbus
+            
+            # Video / DRM
+            libdrm libgbm libva libvdpau
+            
+            # Crypto
+            nss nspr libgcrypt krb5
+            
+            # Compression
+            zlib zstd
+            
+            # Desktop integration
+            hicolor-icon-theme
+
+            qt6.qtserialport 
+            gtk3 
+            pango
+            cairo
+            atk
+            
+            libxcb-util
+            libxcb-cursor
+            libxcb-image
+            libxcb-keysyms
+            libxcb-render-util
+          ];
+          
+          desktopItems = [ desktopItem ];
+
+          unpackPhase = ''
+            dpkg -x $src .
+          '';
+
+          installPhase = ''
+            runHook preInstall
+
+            mkdir -p $out/share/max
+            if [ -d "usr/share/max" ]; then
+              cp -r usr/share/max/* $out/share/max/
+            fi
+
+            # --- 1. УДАЛЕНИЕ max-service ---
+            echo "Removing max-service directory..."
+            rm -rf $out/share/max/bin/max-service
+
+            # --- 1.5 ОЧИСТКА БИТЫХ СИМЛИНКОВ ---
+            echo "Cleaning up broken symlinks caused by max-service removal..."
+            find $out/share/max/lib64 -xtype l -delete
+
+            # --- 2. УДАЛЕНИЕ СТАРОЙ GLIB ---
+            echo "Removing bundled GLib libraries to use system GLib..."
+            rm -f $out/share/max/lib64/libglib-2.0.so*
+            rm -f $out/share/max/lib64/libgobject-2.0.so*
+            rm -f $out/share/max/lib64/libgio-2.0.so*
+            rm -f $out/share/max/lib64/libgmodule-2.0.so*
+            rm -f $out/share/max/lib64/libgthread-2.0.so*
+
+            # --- 3. QT.CONF ---
+            mkdir -p $out/share/max/bin
+            echo "[Paths]" > $out/share/max/bin/qt.conf
+            echo "Prefix = .." >> $out/share/max/bin/qt.conf
+            echo "Libraries = lib64" >> $out/share/max/bin/qt.conf
+            echo "Plugins = plugins" >> $out/share/max/bin/qt.conf
+
+            # --- 4. ПОИСК ИКОНКИ ---
+            mkdir -p $out/share/pixmaps
+            find $out/share/max -iname "max.png" | head -1 | while read icon; do
+              cp "$icon" $out/share/pixmaps/max.png
+            done
+            if [ ! -f "$out/share/pixmaps/max.png" ]; then
+              find . -path "*/pixmaps/max.png" -o -path "*/icons/hicolor/*/apps/max.png" | head -1 | xargs -r cp -t $out/share/pixmaps/max.png || true
+            fi
+
+            runHook postInstall
+          '';
+
+          autoPatchelfSearchPath = [
+            "$out/share/max/lib64"
+          ];
+
+          postFixup = ''
+            # --- 5. WRAPPER ---
+            wrapProgram $out/share/max/bin/max \
+              --set QT_QPA_PLATFORM "wayland;xcb" \
+              --set QT_PLUGIN_PATH "$out/share/max/plugins" \
+              --prefix XDG_DATA_DIRS : "$out/share"
+
+            mkdir -p $out/bin
+            ln -sf $out/share/max/bin/max $out/bin/max
+          '';
+
+          meta = with pkgs.lib; {
+            description = "MAX Messenger (Native NixOS build, no service)";
+            homepage = "https://max.ru";
+            platforms = [ "x86_64-linux" ];
+            maintainers = [ "spiage" ];
+            license = licenses.unfree;
+          };
+        };
       }
-    );    
+    );
 }
